c27aefd3d397e3bf7784dfd0207af326a14a2864
LittleLebowski <o.f.atas@gmail.com>
2022-10-28 18:18:41 +0300

    Created the array of objects from story.txt

--- madlibs.js
+++ madlibs.js
@@ -1,68 +1,69 @@
-/**
- * Complete the implementation of parseStory.
- *
- * parseStory retrieves the story as a single string from story.txt
- * (I have written this part for you).
- *
- * In your code, you are required (please read this carefully):
- * - to return a list of objects
- * - each object should definitely have a field, `word`
- * - each object should maybe have a field, `pos` (part of speech)
- *
- * So for example, the return value of this for the example story.txt
- * will be an object that looks like so (note the comma! periods should
- * be handled in the same way).
- *
- * Input: "Louis[n] went[v] to the store[n], and it was fun[a]."
- * Output: [
- *  { word: "Louis", pos: "noun" },
- *  { word: "went", pos: "verb", },
- *  { word: "to", },
- *  { word: "the", },
- *  { word: "store", pos: "noun" }
- *  { word: "," }
- *  ....
- *
- * There are multiple ways to do this, but you may want to use regular expressions.
- * Please go through this lesson: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/
- */
-
-var array = [{ name1: "value1" }, { name2: "value2" }],
-  object = Object.assign({}, ...array);
-
-console.log(object);
-
-Object.assign({}, ["a", "b", "c"]); // {0:"a", 1:"b", 2:"c"}
+// /**
+//  * Complete the implementation of parseStory.
+//  *
+//  * parseStory retrieves the story as a single string from story.txt
+//  * (I have written this part for you).
+//  *
+//  * In your code, you are required (please read this carefully):
+//  * - to return a list of objects
+//  * - each object should definitely have a field, `word`
+//  * - each object should maybe have a field, `pos` (part of speech)
+//  *
+//  * So for example, the return value of this for the example story.txt
+//  * will be an object that looks like so (note the comma! periods should
+//  * be handled in the same way).
+//  *
+//  * Input: "Louis[n] went[v] to the store[n], and it was fun[a]."
+//  * Output: [
+//  *  { word: "Louis", pos: "noun" },
+//  *  { word: "went", pos: "verb", },
+//  *  { word: "to", },
+//  *  { word: "the", },
+//  *  { word: "store", pos: "noun" }
+//  *  { word: "," }
+//  *  ....
+//  *
+//  * There are multiple ways to do this, but you may want to use regular expressions.
+//  * Please go through this lesson: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/
+//  */
 
 function parseStory(rawStory) {
   // Your code here.
   const spl = (string) => {
-    const bla = string.split(" ");
-    return bla;
+    const splitedStory = string.split(" ");
+    return splitedStory;
   };
+  let arrayOfObjects = [];
   const arr = spl(rawStory);
   const check = arr.map((word) => {
-    if (word.match(/\[v\]/)) {
-      console.log(word);
-    } else if (/\[n\]/.test(word)) {
-      console.log(word);
+    if (/\[v\]/.test(word)) {
+      arrayOfObjects.push({ word: word.replace("[v]", ""), pos: "verb" });
     } else if (/\[a\]/.test(word)) {
-      console.log(word);
+      arrayOfObjects.push({
+        word: word.replace("[a]", ""),
+        pos: "adjective",
+      });
+    } else if (/\[n\]/.test(word)) {
+      arrayOfObjects.push({ word: word.replace("[n]", ""), pos: "noun" });
+    } else {
+      arrayOfObjects.push({ word: word });
     }
   });
-  return arr;
+  const x = JSON.stringify(arrayOfObjects);
+  console.log(arrayOfObjects);
+  return x;
   // This line is currently wrong :)
 }
 
-/**
- * All your other JavaScript code goes here, inside the function. Don't worry about
- * the `then` and `async` syntax for now.
- *
- * You'll want to use the results of parseStory() to display the story on the page.
- */
+// /**
+//  * All your other JavaScript code goes here, inside the function. Don't worry about
+//  * the `then` and `async` syntax for now.
+//  *
+//  * You'll want to use the results of parseStory() to display the story on the page.
+//  */
 getRawStory()
   .then(parseStory)
   .then((processedStory) => {
-    document.getElementById("demo").innerHTML = processedStory;
+    document.getElementById("demo").innerHTML = processedStory; // id demo was not on the html i put it there "omer"
     console.log(processedStory);
   });
