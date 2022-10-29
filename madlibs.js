820a549ace26c71f29909e38d1adcc46faea14d1
LittleLebowski <o.f.atas@gmail.com>
2022-10-29 21:50:37 +0300

    reformed the structure

--- madlibs.js
+++ madlibs.js
@@ -1,19 +1,18 @@
-<<<<<<< HEAD
 /**
  * Complete the implementation of parseStory.
- * 
+ *
  * parseStory retrieves the story as a single string from story.txt
  * (I have written this part for you).
- * 
+ *
  * In your code, you are required (please read this carefully):
  * - to return a list of objects
  * - each object should definitely have a field, `word`
  * - each object should maybe have a field, `pos` (part of speech)
- * 
+ *
  * So for example, the return value of this for the example story.txt
  * will be an object that looks like so (note the comma! periods should
  * be handled in the same way).
- * 
+ *
  * Input: "Louis[n] went[v] to the store[n], and it was fun[a]."
  * Output: [
  *  { word: "Louis", pos: "noun" },
@@ -23,53 +22,21 @@
  *  { word: "store", pos: "noun" }
  *  { word: "," }
  *  ....
- * 
+ *
  * There are multiple ways to do this, but you may want to use regular expressions.
  * Please go through this lesson: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/
  */
 
-  // Your code here.
-  
-  // This is examplePull
-  /**
-   * All your other JavaScript code goes here, inside the function. Don't worry about
-   * the `then` and `async` syntax for now.
-   * 
-   * You'll want to use the results of parseStory() to display the story on the page.
-   */
-    
-=======
-// /**
-//  * Complete the implementation of parseStory.
-//  *
-//  * parseStory retrieves the story as a single string from story.txt
-//  * (I have written this part for you).
-//  *
-//  * In your code, you are required (please read this carefully):
-//  * - to return a list of objects
-//  * - each object should definitely have a field, `word`
-//  * - each object should maybe have a field, `pos` (part of speech)
-//  *
-//  * So for example, the return value of this for the example story.txt
-//  * will be an object that looks like so (note the comma! periods should
-//  * be handled in the same way).
-//  *
-//  * Input: "Louis[n] went[v] to the store[n], and it was fun[a]."
-//  * Output: [
-//  *  { word: "Louis", pos: "noun" },
-//  *  { word: "went", pos: "verb", },
-//  *  { word: "to", },
-//  *  { word: "the", },
-//  *  { word: "store", pos: "noun" }
-//  *  { word: "," }
-//  *  ....
-//  *
-//  * There are multiple ways to do this, but you may want to use regular expressions.
-//  * Please go through this lesson: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/
-//  */
+// Your code here.
 
-  
->>>>>>> f99330fbe703f51a471d80139f7eff6e04d49c75
+// This is examplePull
+/**
+ * All your other JavaScript code goes here, inside the function. Don't worry about
+ * the `then` and `async` syntax for now.
+ *
+ * You'll want to use the results of parseStory() to display the story on the page.
+ */
+
 function parseStory(rawStory) {
   // Your code here.
   const spl = (string) => {
@@ -91,159 +58,68 @@ function parseStory(rawStory) {
     } else {
       arrayOfObjects.push({ word: word });
     }
-<<<<<<< HEAD
     return arrayOfObjects;
   });
- 
-
-        //document.getElementById("demo").innerHTML = processedStory; // id demo was not on the html i put it there "omer"
-    
-  
-    
-        const x = JSON.stringify(arrayOfObjects);
-        console.log(arrayOfObjects);
-        forInput(arrayOfObjects)
-        return x;
+  const x = JSON.stringify(arrayOfObjects);
+  console.log(arrayOfObjects);
+  forInput(arrayOfObjects);
+  return x;
 }
-      
- 
-    //document.getElementById("demo").innerHTML = processedStory; // id demo was not on the html i put it there "omer"
-    
-     function forInput(array){
-      const madLibsEdit = document.querySelector('.madLibsEdit')
-      const madLibsPreview = document.querySelector('.madLibsPreview')
-  
-function createInputStory(place, pholder){
-     place.innerHTML +=`<span> <input type='text' name='type' value='' placeholder=${pholder}> </span>`
-      }
-      function createOutputStory(place, pholder){
-        place.innerHTML +=`<span> <input type='text' name='type' value='' placeholder=${pholder} readonly> </span>`
-      }
-    array.forEach(object => {
-      if(object.pos){
-      createInputStory(madLibsEdit,  object.pos);
-        createOutputStory(madLibsPreview,  object.pos)
-      }else{
-        madLibsEdit.innerHTML += `${object.word} `
-        madLibsPreview.innerHTML += `${object.word} `
-            }
-  
-        document.querySelectorAll(`.madLibsEdit input`).forEach((input,index) => {
-          input.addEventListener('input', e => { document.querySelectorAll('.madLibsPreview input')[index].value = e.target.value 
-                })
-            })
-    })
+
+function forInput(array) {
+  const madLibsEdit = document.querySelector(".madLibsEdit");
+  const madLibsPreview = document.querySelector(".madLibsPreview");
+
+  function createInputStory(place, pholder) {
+    place.innerHTML += `<span> <input type='text' name='type' value='' placeholder=${pholder}> </span>`;
   }
- 
-  
-function setupSound() 
-{ 
-  var obj = document.createElement("object"); 
-  obj.width="0px"; 
-  obj.height="0px"; 
-  obj.type = "audio/mp3"; 
+  function createOutputStory(place, pholder) {
+    place.innerHTML += `<span> <input type='text' name='type' value='' placeholder=${pholder} readonly> </span>`;
+  }
+  array.forEach((object) => {
+    if (object.pos) {
+      createInputStory(madLibsEdit, object.pos);
+      createOutputStory(madLibsPreview, object.pos);
+    } else {
+      madLibsEdit.innerHTML += `${object.word} `;
+      madLibsPreview.innerHTML += `${object.word} `;
+    }
+
+    document.querySelectorAll(`.madLibsEdit input`).forEach((input, index) => {
+      input.addEventListener("input", (e) => {
+        document.querySelectorAll(".madLibsPreview input")[index].value =
+          e.target.value;
+      });
+    });
+  });
+}
+
+function setupSound() {
+  var obj = document.createElement("object");
+  obj.width = "0px";
+  obj.height = "0px";
+  obj.type = "audio/mp3";
   obj.data = "./sounds/free!.mp3";
   obj.setAttribute("id", "bgsound1");
-  var body = document.getElementsByTagName("body")[0]; 
-  body.appendChild(obj); 
-      
-} 
+  var body = document.getElementsByTagName("body")[0];
+  body.appendChild(obj);
+}
 function Create() {
-            
   // Create audio element.
-  var m = document.createElement(
-    "AUDIO");
+  var m = document.createElement("AUDIO");
 
   if (m.canPlayType("audio/mpeg")) {
-      m.setAttribute("src", "sound/free!.mp3");
+    m.setAttribute("src", "sound/free!.mp3");
   } else {
-      m.setAttribute("src", "bells.ogg");
+    m.setAttribute("src", "bells.ogg");
   }
 
   m.setAttribute("controls", "controls");
   document.body.appendChild(m);
-
 }
 
-
-getRawStory()
-  .then(parseStory)
-  {
-    //document.getElementById("demo").innerHTML = processedStory; // id demo was not on the html i put it there "omer"
-    setupSound()
-    console.log("any thing");
-  }
-
-        
-      
-
-=======
-  });
-    const x = JSON.stringify(arrayOfObjects);
-  console.log(arrayOfObjects);
-  forInput(arrayOfObjects)
-  return x;
-
+getRawStory().then(parseStory);
+{
+  setupSound();
+  console.log("any thing");
 }
-  
-  
-  
-  function forInput(object) {
-    
-    for(let i=0; i<object.length; i++){
-      //const object = object[i]
-      const span = (document.getElementById(
-        "demo").innerHTML += `<span class=“spanclass” id=" ${object[i].word}">${object[i].word}</span>`);
-  
-      //span.appendChild() 
-
-
-      if('pos'in object[i]){
-        const input = (document.getElementById("demo").innerHTML += `<input class=“inputclass” id=" ${object[i].word}">${object[i].word}</input>`);
-        input.id=`${i}`
-        
-  
-
-       /*const input = document.createElement("input");
-        input.setAttribute("placeholder", `${object[i].pos}`);
-        input.id=`${i}`*/
-       
-
-
-
-      }
-
-    }
-
-  }
-  
-  
-  
- 
-
-  
-  
-  
-
- 
-  
-  // This line is currently wrong :)
-  
-
-
-// /**
-//  * All your other JavaScript code goes here, inside the function. Don't worry about
-//  * the `then` and `async` syntax for now.
-//  *
-//  * You'll want to use the results of parseStory() to display the story on the page.
-//  */
-
-getRawStory()
-  .then(parseStory)
-  .then((processedStory) => {
-    //document.getElementById("demo").innerHTML = processedStory; // id demo was not on the html i put it there "omer"
-    
-    console.log(processedStory);
-  });
-  
->>>>>>> f99330fbe703f51a471d80139f7eff6e04d49c75
