72c9c1fd0620cd1563f0c2b4684e057335c0a838
sara sallat <sallatsara@gmail.com>
2022-10-29 20:39:24 +0300

    new update

--- madlibs.js
+++ madlibs.js
@@ -26,17 +26,120 @@
  * There are multiple ways to do this, but you may want to use regular expressions.
  * Please go through this lesson: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/
  */
+
+  // Your code here.
+  
+  // This is examplePull
+  /**
+   * All your other JavaScript code goes here, inside the function. Don't worry about
+   * the `then` and `async` syntax for now.
+   * 
+   * You'll want to use the results of parseStory() to display the story on the page.
+   */
+    
 function parseStory(rawStory) {
   // Your code here.
-  return {}; // This line is currently wrong :)
+  const spl = (string) => {
+    const splitedStory = string.split(" ");
+    return splitedStory;
+  };
+  let arrayOfObjects = [];
+  const arr = spl(rawStory);
+  const check = arr.map((word) => {
+    if (/\[v\]/.test(word)) {
+      arrayOfObjects.push({ word: word.replace("[v]", ""), pos: "verb" });
+    } else if (/\[a\]/.test(word)) {
+      arrayOfObjects.push({
+        word: word.replace("[a]", ""),
+        pos: "adjective",
+      });
+    } else if (/\[n\]/.test(word)) {
+      arrayOfObjects.push({ word: word.replace("[n]", ""), pos: "noun" });
+    } else {
+      arrayOfObjects.push({ word: word });
+    }
+    return arrayOfObjects;
+  });
+ 
+
+        //document.getElementById("demo").innerHTML = processedStory; // id demo was not on the html i put it there "omer"
+    
+  
+    
+        const x = JSON.stringify(arrayOfObjects);
+        console.log(arrayOfObjects);
+        forInput(arrayOfObjects)
+        return x;
+}
+      
+ 
+    //document.getElementById("demo").innerHTML = processedStory; // id demo was not on the html i put it there "omer"
+    
+     function forInput(array){
+      const madLibsEdit = document.querySelector('.madLibsEdit')
+      const madLibsPreview = document.querySelector('.madLibsPreview')
+  
+function createInputStory(place, pholder){
+     place.innerHTML +=`<span> <input type='text' name='type' value='' placeholder=${pholder}> </span>`
+      }
+      function createOutputStory(place, pholder){
+        place.innerHTML +=`<span> <input type='text' name='type' value='' placeholder=${pholder} readonly> </span>`
+      }
+    array.forEach(object => {
+      if(object.pos){
+      createInputStory(madLibsEdit,  object.pos);
+        createOutputStory(madLibsPreview,  object.pos)
+      }else{
+        madLibsEdit.innerHTML += `${object.word} `
+        madLibsPreview.innerHTML += `${object.word} `
+            }
+  
+        document.querySelectorAll(`.madLibsEdit input`).forEach((input,index) => {
+          input.addEventListener('input', e => { document.querySelectorAll('.madLibsPreview input')[index].value = e.target.value 
+                })
+            })
+    })
+  }
+ 
+  
+function setupSound() 
+{ 
+  var obj = document.createElement("object"); 
+  obj.width="0px"; 
+  obj.height="0px"; 
+  obj.type = "audio/mp3"; 
+  obj.data = "./sounds/free!.mp3";
+  obj.setAttribute("id", "bgsound1");
+  var body = document.getElementsByTagName("body")[0]; 
+  body.appendChild(obj); 
+      
+} 
+function Create() {
+            
+  // Create audio element.
+  var m = document.createElement(
+    "AUDIO");
+
+  if (m.canPlayType("audio/mpeg")) {
+      m.setAttribute("src", "sound/free!.mp3");
+  } else {
+      m.setAttribute("src", "bells.ogg");
+  }
+
+  m.setAttribute("controls", "controls");
+  document.body.appendChild(m);
+
 }
 
-/**
- * All your other JavaScript code goes here, inside the function. Don't worry about
- * the `then` and `async` syntax for now.
- * 
- * You'll want to use the results of parseStory() to display the story on the page.
- */
-getRawStory().then(parseStory).then((processedStory) => {
-  console.log(processedStory);
-});
+
+getRawStory()
+  .then(parseStory)
+  {
+    //document.getElementById("demo").innerHTML = processedStory; // id demo was not on the html i put it there "omer"
+    setupSound()
+    console.log("any thing");
+  }
+
+        
+      
+
